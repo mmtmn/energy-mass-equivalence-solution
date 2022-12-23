87f894f9d305dd9f5a72203778c39aebe68f8950
mmtmn <42742390+mmtmn@users.noreply.github.com>
2022-12-23 16:46:24 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+import math
+c=299792458
+
+def electric_potential_energy(q, V):
+    return q*V
+
+def binding_energy(Z, m_p, m_nucleus):    
+    return Z*m_p*c**2 - m_nucleus*c**2
+
+def kinetic_energy(p, m_0):
+  return math.sqrt(p**2*c**2 + m_0**2*c**4) - m_0*c**2
+
+def photon_energy(h, f):
+  return h*f
+
+def energy_from_mass(m):
+  return m*c**2
+
+def combined_equation(m, g, h, q, V, Z, m_p, m_nucleus, p, m_0, h, f):
+  E_g = m*g*h
+  E_e = q*V
+  E_b = Z*m_p*c**2 - m_nucleus*c**2
+  E_k = math.sqrt(p**2*c**2 + m_0**2*c**4) - m_0*c**2
+  E = h*f
+  E_total = E + E_g + E_e + E_b + E_k
+  return E_total
