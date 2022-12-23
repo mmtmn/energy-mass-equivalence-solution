23193e5f223cec3b83f97267b05886b114af5e69
mmtmn <42742390+mmtmn@users.noreply.github.com>
2022-12-23 17:27:32 -0300

    Update main.py

--- main.py
+++ main.py
@@ -24,3 +24,19 @@ def combined_equation(m, g, h, q, V, Z, m_p, m_nucleus, p, m_0, h, f):
   E = h*f
   E_total = E + E_g + E_e + E_b + E_k
   return E_total
+
+###
+# Bruteforcing the solution :)
+###
+
+import itertools
+
+# Create a list of the characters
+characters = ["E_g", "=", "m","*","g","*","h","E_e", "=", "q","*","V", "E_b", "=", "Z","*","m_p","*","c","**","2", "-", "m_nucleus","*","c","**","2","E_k", "=", "math.sqrt","(","p","**","2","*","c","**","2", "+", "m_0","**","2","*","c","**","4",")", "-", "m_0","*","c","**","2", "E", "=", "h","*","f","E_total", "=", "E", "+", "E_g", "+", "E_e", "+", "E_b", "+", "E_k",]
+
+# Generate all the permutations of length 4 of the characters
+combinations = itertools.permutations(characters, len(characters))
+
+# Print the combinations
+for combination in combinations:
+    print(combination)
